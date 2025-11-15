10ea1c9cb5777e184b09c9e5944b1a7066c9d975
timreamax <maxhelaku@gmail.com>
2025-11-15 10:00:08 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    ALLOWED_ORIGINS = [
+    ALLOWED_ORIGINS: list[str] = [
         "https://lajmifundit.eu",
         "https://rea.lajmifundit.eu",
-        "http://localhost:5173",
+        "http://localhost:5173"
     ]
 
     class Config:
         env_file = ".env"
         extra = "ignore"
 
-
 settings = Settings()
+
